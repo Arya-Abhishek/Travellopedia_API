e644e4d23ada4a4ab05482c513a0d427f1510b6d
Abhishek <arya2001abhishek@gmail.com>
2022-02-20 17:03:43 +0530

    add ErrorResponse class extending error and asyncHandler

--- controllers/companies.js
+++ controllers/companies.js
@@ -1,92 +1,72 @@
 const Company = require('../models/Company')
+const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
 
 // @desc      Get all companies
 // @route     GET /api/v1/companies
 // @access    Public
-exports.getCompanies = async (req, res, next) => {
-  try {
-    const companies = await Company.find({});
+exports.getCompanies = asyncHandler(async (req, res, next) => {
+  const companies = await Company.find({});
 
-    res.status(200).json({
-      success: true,
-      count: companies.length,
-      data: companies
-    })
-  } catch (err) {
-    console.log(err)
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: companies.length,
+    data: companies
+  })
+});
 
 // @desc      Get Single Company
 // @route     GET /api/v1/companies/:id
 // @access    Public
-exports.getCompany = async (req, res, next) => {
-  try {
-    const company = await Company.findById(req.params.id);
+exports.getCompany = asyncHandler(async (req, res, next) => {
+  const company = await Company.findById(req.params.id);
 
-    if (!company) {
-      return res.status(400).json({
-        success: false
-      })
-    }
-
-    res.status(200).json({
-      success: true,
-      data: company
-    })
-  } catch (err) {
-    next(err)
+  if (!company) {
+    return next(new ErrorResponse(`Company not found with id ${req.params.id}`, 404))
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: company
+  })
+});
 
 // @desc      Create Company
 // @route     POST /api/v1/companies
 // @access    Private
-exports.addCompany = async (req, res, next) => {
+exports.addCompany = asyncHandler(async (req, res, next) => {
   console.log(req.body)
-  try {
-    const company = await Company.create(req.body);
+  const company = await Company.create(req.body);
 
-    res.status(200).json({
-      success: true,
-      data: company
-    })
-  } catch (err) {
-    console.log(err)
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: company
+  })
+});
 
 // @desc      Update Company
 // @route     PUT /api/v1/companies/:id
 // @access    Private
-exports.updateCompany = async (req, res, next) => {
-  try {
-    const company = await Company.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+exports.updateCompany = asyncHandler(async (req, res, next) => {
+  const company = await Company.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    res.status(200).json({
-      success: true,
-      data: company
-    })
-  } catch (err) {
-    console.log(err)
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: company
+  })
+});
 
 // @desc      Delete Company
 // @route     DELETE /api/v1/companies/:id
 // @access    Private
-exports.deleteCompany = async (req, res, next) => {
-  try {
-    await Company.deleteOne({ "_id": req.params.id });
+exports.deleteCompany = asyncHandler(async (req, res, next) => {
+  await Company.deleteOne({ "_id": req.params.id });
 
-    res.status(200).json({
-      success: true,
-      data: {}
-    })
-  } catch (err) {
-    console.log(err)
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: {}
+  })
+});
