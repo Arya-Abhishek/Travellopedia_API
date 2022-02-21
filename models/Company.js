5effa59ff3e35bf43502e393f1fe2a60302520e2
Arya-Abhishek <arya2001abhishek@gmail.com>
2022-02-22 00:39:38 +0530

    add node-geocoder and populate location geoJSON field in pre middleware

--- models/Company.js
+++ models/Company.js
@@ -1,73 +1,97 @@
-const mongoose = require('mongoose')
-const slugify = require('slugify')
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+const geocoder = require("../utils/geocode")
 
 const CompanySchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, 'Please add a name'],
-		unique: true,
-		trim: true,
-		maxlength: [50, 'Name cannot be more than 50 characters']
-	},
-	slug: String,
-	description: {
-		type: String,
-		required: [true, 'Please add a description'],
-		maxlength: [500, 'Description cannot be more than 500 characters']
-	},
-	website: {
-		type: String,
-		match: [
-			/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-			'Please use a valid URL with HTTP or HTTPS'
-		]
-	},
-	phone: {
-		type: String,
-		maxlength: [20, 'Phone number can not be longer than 20 characters']
-	},
-	email: {
-		type: String,
-		match: [
-			/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-			'Please add a valid email'
-		]
-	},
-	address: {
-		type: String,
-		required: [true, 'Please add an address']
-	},
-	location: {
-		// GeoJSON Point
-		type: {
-			type: String,
-			enum: ['Point']
-		},
-		coordinates: {
-			type: [Number],
-			index: '2dsphere'
-		},
-		formattedAddress: String,
-		street: String,
-		city: String,
-		state: String,
-		zipcode: String,
-		country: String
-	},
-	averageRating: {
-		type: Number,
-		min: [1, 'Rating must be at least 1'],
-		max: [10, 'Rating must can not be more than 10']
-	},
-	averageCost: Number,
-	photo: {
-		type: String,
-		default: 'no-photo.jpg'
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now
-	}
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name cannot be more than 50 characters"],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, "Please add a description"],
+    maxlength: [500, "Description cannot be more than 500 characters"],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      "Please use a valid URL with HTTP or HTTPS",
+    ],
+  },
+  phone: {
+    type: String,
+    maxlength: [20, "Phone number can not be longer than 20 characters"],
+  },
+  email: {
+    type: String,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email",
+    ],
+  },
+  address: {
+    type: String,
+    required: [true, "Please add an address"],
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ["Point"],
+    },
+    coordinates: {
+      type: [Number],
+      index: "2dsphere",
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  averageRating: {
+    type: Number,
+    min: [1, "Rating must be at least 1"],
+    max: [10, "Rating must can not be more than 10"],
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: "no-photo.jpg",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+CompanySchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+CompanySchema.pre("save", async function(next) {
+	const loc = await geocoder.geocode(this.address)
+
+	this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  };
+
+  this.address = undefined;
+  next();
 })
 
-module.exports = mongoose.model('Company', CompanySchema)
+module.exports = mongoose.model("Company", CompanySchema);
